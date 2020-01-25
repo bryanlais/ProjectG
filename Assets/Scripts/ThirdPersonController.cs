a9f3382d53e3e799b2efcada80a36aeace45dc49
bryanlai00 <bryanjl22@hotmail.com>
2020-01-24 21:52:10 -0500

    Upload 3D Unity Project G

---
+++ Assets/Scripts/ThirdPersonController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonController : MonoBehaviour
+{
+    public Animator animator;
+    public float Speed;
+
+    void Start()
+    {
+        animator = this.gameObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            PlayerAttack();
+        }
+        else
+        {
+            PlayerMovement();
+        }
+    }
+
+    void PlayerMovement()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        Vector3 playerMovement = new Vector3(hor, 0f, ver) * Speed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
+    }
+
+    void PlayerAttack()
+    {
+        animator.SetTrigger("Attack");
+    }
+}
