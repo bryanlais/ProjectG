c011f4c40f94a7191c50685f110187c068d54fb6
Bryan Lai <bryanjl22@hotmail.com>
2020-01-25 00:39:43 -0500

    Add Title Screen

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody player;
+
+    void Update()
+    {
+        if(player.position.y < -4.6)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }    
+    }
+}
---
+++ Assets/Scripts/StartManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartManager : MonoBehaviour
+{
+    public void startGame()
+    {
+        Debug.Log("Started the game!");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void quitGame()
+    {
+        Debug.Log("Quit the game!");
+        Application.Quit();
+    }
+
+    public void instructions()
+    {
+        Debug.Log("Instructions!");
+    }
+}
